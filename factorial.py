2bd8a056c65164590579f56314cb13f1eb1e7e53
linangell <angel110sky@gmail.com>
2023-12-19 20:31:22 +0400

    factorial

--- factorial.py
+++ factorial.py
@@ -1,9 +1,9 @@
 def factorial(k):
     result = 1
-    for i in range(2, k + 1):
+    for i in range(0, k + 1):
         result *= i
     return result
-#
+
 # k = int(input ("Enter a value: "))
 # result = factorial(k)
 # print("The factorial of", k," is:", result)
--- main.py
+++ main.py
@@ -2,18 +2,43 @@ import tkinter as tk
 import fibonacci
 import factorial
 
-print ('Which task would you like to perform:')
-print('1: Find	the	factorial	')
-print('2: Find	the	fibonacci value	')
-m = input ("Enter the number of the task you'd like to perform : ")
-if m == '1':
-  v = input ("Enter the integer you'd like to find its factorial: ")
+def calculate_factorial():
+    v = int(entry.get())
+    result = factorial(v)
+    result_label.config(text=result)
 
-  print('the factorial of' , v , 'is' , n )
-elif m == '2':
-  l = input ("Enter the sequences position of the fibonacci value: ")
 
-  print('the value pf posion' , v , 'in the fibonachi value' , k )
+window = tk.Tk()
+window.title("ff")
+
+label = tk.Label(window, text="Which task would you like to perform:")
+label.pack()
+
+factorial_button = tk.Button(window, text="Find the factorial", command=calculate_factorial)
+factorial_button.pack()
+
+fibonacci_button = tk.Button(window, text="Find the Fibonacci value", command=fibonacci)
+fibonacci_button.pack()
+
+entry = tk.Entry(window)
+entry.pack()
+
+result_label = tk.Label(window, text="")
+result_label.pack()
+window.mainloop()
+
+# print ('Which task would you like to perform:')
+# print('1: Find	the	factorial	')
+# print('2: Find	the	fibonacci value	')
+# m = input ("Enter the number of the task you'd like to perform : ")
+# if m == '1':
+#   v = input ("Enter the integer you'd like to find its factorial: ")
+#
+#   print('the factorial of' , v , 'is' , n )
+# elif m == '2':
+#   l = input ("Enter the sequences position of the fibonacci value: ")
+#
+#   print('the value pf posion' , v , 'in the fibonachi value' , k )
 
 
 
